36ce72cfa4c2ef9c2d4d532afeabaf87365aab62
abrahamuchos <agonzalez@wayuinc.com>
2023-03-13 18:41:33 -0400

    Add Router Provider, createBrowseRouter and Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom";
 import './App.css'
 
+//Layout
+import RootLayout from "./layouts/RootLayout.jsx";
 //Pages
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+    </Route>
+  )
+);
+
 function App() {
 
   return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jabarouter</h1>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='about'>About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home/>}/>
-          <Route path="about" element={<About/>}/>
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   );
 }
 
