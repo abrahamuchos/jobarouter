b9d9e52f637b5cab570ab9f101f303a2580fc118
abrahamuchos <agonzalez@wayuinc.com>
2023-03-14 19:50:40 -0400

    Add loader(react router)

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import './App.css'
 //Layout
 import RootLayout from "./layouts/RootLayout.jsx";
 import HelpLayout from "./layouts/HelpLayout.jsx";
+import CareersLayout from "./layouts/CareersLayout.jsx";
 
 //Pages
 import Home from "./pages/Home.jsx";
@@ -16,19 +17,29 @@ import About from "./pages/About.jsx";
 import Faq from "./pages/help/Faq.jsx";
 import Contact from "./pages/help/Contact.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import Careers, { careersLoader } from "./pages/Careers.jsx";
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RootLayout />} >
-      <Route index element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='help' element={<HelpLayout />} >
-        <Route path='faq' element={<Faq />} />
-        <Route path='contact' element={<Contact />} />
+    <Route path='/' element={<RootLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+
+      <Route path='help' element={<HelpLayout/>}>
+        <Route path='faq' element={<Faq/>}/>
+        <Route path='contact' element={<Contact/>}/>
       </Route>
 
-      <Route path="*" element={<NotFound />} />
+      <Route path="careers" element={<CareersLayout/>}>
+        <Route
+          index
+          element={<Careers/>}
+          loader={careersLoader}
+        />
+      </Route>
+
+      <Route path="*" element={<NotFound/>}/>
     </Route>
   )
 );
