994e12d9da0c4ffc575b9ecbe742be97d7171263
abrahamuchos <agonzalez@wayuinc.com>
2023-03-25 16:05:53 -0400

    Add actions to form

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import CareersLayout from "./layouts/CareersLayout.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Faq from "./pages/help/Faq.jsx";
-import Contact from "./pages/help/Contact.jsx";
+import Contact, { contactAction } from "./pages/help/Contact.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Careers, { careersLoader } from "./pages/careers/Careers.jsx";
 import CareerDetails, { careerDetailsLoader } from "./pages/careers/CareerDetails.jsx";
@@ -30,7 +30,7 @@ const router = createBrowserRouter(
 
       <Route path='help' element={<HelpLayout/>}>
         <Route path='faq' element={<Faq/>}/>
-        <Route path='contact' element={<Contact/>}/>
+        <Route path='contact' element={<Contact/>} action={contactAction}/>
       </Route>
 
       <Route path="careers" element={<CareersLayout/>} errorElement={<CareersError/>}>
